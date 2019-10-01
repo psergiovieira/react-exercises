1560633db968d7349ead2758b483cfd4a1def449
Paulo Vieira <paulo.sergiovieira-ext@ab-inbev.com>
2019-10-01 09:10:05 -0300

    adding validation to the form

--- src/CreateBeerFormikForm.tsx
+++ src/CreateBeerFormikForm.tsx
@@ -7,17 +7,34 @@ import {
   Field
 } from "formik";
 import BeerEntity from "./BeerEntity";
+import * as Yup from "yup";
 
 interface Props {
   onSubmit: (data: BeerEntity) => void;
 }
+
+const beerFormSchema = Yup.object().shape({
+  beerName: Yup.string()
+    .min(2, "Too Short!")
+    .max(50, "Too Long!")
+    .required("Required"),
+  beerType: Yup.string()
+    .min(3, "Too Short!")
+    .max(6, "Too Long!")
+    .required("Required"),
+  hasCorn: Yup.boolean().required("Required"),
+  ingredients: Yup.string()
+    .min(5)
+    .max(200)
+    .required("Required")
+});
 
 const validate = (values: BeerEntity): void => {
   console.log("validate");
 };
 
 function CreateBeerFormikForm(props: Props) {
-  const beersTypes = ["Larger", "Ale", "Stout", "Malt"];
+  const beersTypes = ["", "Larger", "Ale", "Stout", "Malt"];
   const onSubmit = (values: BeerEntity, actions: FormikActions<BeerEntity>) => {
     props.onSubmit(values);
     actions.setSubmitting(false);
@@ -35,9 +52,12 @@ function CreateBeerFormikForm(props: Props) {
           name="beerName"
           placeholder="Beer Name"
         />
+        {propsFormik.errors.beerName && propsFormik.touched.beerName ? (
+          <div>{propsFormik.errors.beerName}</div>
+        ) : null}
 
         <br />
-        <label id="idLabelBeerType" htmlFor="idBeerType">       
+        <label id="idLabelBeerType" htmlFor="idBeerType">
           Beer Type:
         </label>
         <Field id="idBeerType" name="beerType" component="select">
@@ -47,7 +67,9 @@ function CreateBeerFormikForm(props: Props) {
             </option>
           ))}
         </Field>
-
+        {propsFormik.errors.beerType && propsFormik.touched.beerType ? (
+          <div>{propsFormik.errors.beerType}</div>
+        ) : null}
         <br />
         <label id="idHasCornLabel" htmlFor="idHasCorn">
           Has corn:{" "}
@@ -56,10 +78,17 @@ function CreateBeerFormikForm(props: Props) {
 
         <br />
         <label id="idLabelIdIngredients" htmlFor="idIngredients">
-          {" "}
           Ingredients:{" "}
         </label>
-        <Field  component="textarea" rows="4" id="idIngredients" name="ingredients" />
+        <Field
+          component="textarea"
+          rows="4"
+          id="idIngredients"
+          name="ingredients"
+        />
+        {propsFormik.errors.ingredients && propsFormik.touched.ingredients ? (
+          <div>{propsFormik.errors.ingredients}</div>
+        ) : null}
         <br />
 
         <button type="submit">Submit</button>
@@ -76,7 +105,7 @@ function CreateBeerFormikForm(props: Props) {
         ingredients: ""
       }}
       onSubmit={onSubmit}
-      validate={validate}
+      validationSchema={beerFormSchema}
       render={renderHtml}
     />
   );
